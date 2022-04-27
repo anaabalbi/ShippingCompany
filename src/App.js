ff8d072feb17af1269a70511fdafd7e777c9c48d
anaabalbi <anaabalbi@gmail.com>
2022-04-27 09:46:07 -0300

    atualizando o App com as routes e o Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SearchPage } from './Pages/search/SearchPage.jsx';
+import Home from './Pages/home/PageHome';
+import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header></Header>
+
+        <Routes>
+          <Route path={'/'} element={<Home></Home>}></Route>
+          <Route path='/search/:id' element={<SearchPage></SearchPage>}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
