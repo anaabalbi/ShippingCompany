dc89785006ede2074315a8cab9e14f9d029b4bae
anaabalbi <anaabalbi@gmail.com>
2022-04-27 10:53:55 -0300

    import da logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { SearchModalContext } from '../../Provider/context-modal';
+import logoExpress from '../../assets/logo/express.png';
 import ButtonSearch from '../SearchButton/SearchButton';
 import SearchModal from '../SearchModal/SearchModal';
 import { Nav, Logo, Div, BoxButton, Button } from './style-header';
@@ -11,7 +12,13 @@ const Header = () => {
 
   return (
     <Nav>
-      <Logo></Logo>
+      <Logo>
+        <img
+          src={logoExpress}
+          alt='Nome da transpordadora, Express.JS com uma caixa de papelão aberta'
+          height='90px'
+        />
+      </Logo>
 
       <Div>
         <Link to='/contato'>Contato</Link>
