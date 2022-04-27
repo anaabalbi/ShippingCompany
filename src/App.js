d307ba4fd5a47bdc26f58212dada1a1cb39aff3f
anaabalbi <anaabalbi@gmail.com>
2022-04-27 10:00:54 -0300

    modificando a rota da pagina busca e os caminhos de importação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SearchPage } from './Pages/search/SearchPage.jsx';
-import Home from './Pages/home/PageHome';
-import Header from './components/Header/Header.jsx';
+import { SearchPage } from './Pages/Busca/Busca';
+import Home from './Pages/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
@@ -13,7 +13,10 @@ function App() {
 
         <Routes>
           <Route path={'/'} element={<Home></Home>}></Route>
-          <Route path='/search/:id' element={<SearchPage></SearchPage>}></Route>
+          <Route
+            path='/busca/:code'
+            element={<SearchPage></SearchPage>}
+          ></Route>
         </Routes>
       </Router>
     </>
