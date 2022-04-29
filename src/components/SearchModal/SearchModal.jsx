11738560752f61e17aef791565742f7790f8d438
anaabalbi <anaabalbi@gmail.com>
2022-04-29 11:37:47 -0300

    import hook use api e criando validação deo code tracking

--- src/components/SearchModal/SearchModal.jsx
+++ src/components/SearchModal/SearchModal.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import { SearchModalContext } from '../../Provider/context-modal';
+import { useApi } from '../../Hooks/useApi';
 
 import {
   Overlay,
@@ -13,23 +13,22 @@ import {
   InputContainer,
   Footer,
   CheckIcon,
+  Alert,
 } from './style-modal';
 
 const SearchModal = () => {
-  const { handleClose, codeHandler, codeTracking } =
+  const { handleClose, codeHandler, codeTracking, setData } =
     useContext(SearchModalContext);
+  const [alert, setAlert] = useState(1);
   const navigate = useNavigate();
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  const { data } = useApi(
+    `//transportadora-pedido.herokuapp.com/address/codetracking/${codeTracking}`
+  );
 
-    console.log(codeTracking);
-    if (
-      codeTracking === '' &&
-      codeTracking.length < 7 &&
-      codeTracking === undefined
-    ) {
-      alert('digite o código de rastreio');
+  function HandleSubmit(e) {
+    if (codeTracking === '' || codeTracking.length < 7) {
+      return setAlert(0);
     } else {
       navigate('/busca/' + codeTracking);
       handleClose();
@@ -47,18 +46,19 @@ const SearchModal = () => {
         </Header>
         <FormContainer>
           <InputContainer>
-            <label htmlFor='codeTracking'>Código de Rastreio</label>
+            <label>Código de Rastreio</label>
             <input
-              id='codeTracking'
-              type='text'
+              type='type'
               placeholder='Insira o código de rastreio do pedido'
               onChange={codeHandler}
-              required='required'
             />
+            <Alert erro={alert}>
+              <p> Entre com o código de rastreio, ele contém 7 digitos</p>
+            </Alert>
           </InputContainer>
         </FormContainer>
         <Footer>
-          <button type='submit' value='submit' onClick={handleSubmit}>
+          <button type='submit' value='submit' onClick={HandleSubmit}>
             <CheckIcon />
           </button>
         </Footer>
