181fd8530a1d1a7e09d01cd8b8af9b3470c2ac55
anaabalbi <anaabalbi@gmail.com>
2022-04-30 19:07:43 -0300

    atulizando o componente busca

--- src/Pages/Busca/Busca.jsx
+++ src/Pages/Busca/Busca.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PackageTracking from '../../components/PackageTracking/PackageTracking';
+import { useApi } from '../../Hooks/useApi';
+import { SearchModalContext } from '../../Provider/context-modal';
 
 const Busca = () => {
+  const { codeTracking, setCodeTracking } = useContext(SearchModalContext);
+  localStorage.getItem('codeTrackingStorage', codeTracking);
+
   return (
     <>
       <PackageTracking></PackageTracking>
