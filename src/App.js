762eb87e834e44ae537162cb9b781ef9c3a3ab6a
anaabalbi <anaabalbi@gmail.com>
2022-04-30 19:02:00 -0300

    importando o header e o footer no app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Animation from './components/Animation/Animation.jsx';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Busca from './Pages/Busca/Busca.jsx';
-import Home from './Pages/Home/Home.jsx';
-import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+
+import Footer from './components/Footer/Footer';
+import HeaderHome from './components/HeaderHome/HeaderHome.js';
 
 function App() {
   return (
     <>
       <Router>
-        <Header></Header>
-
+        <HeaderHome></HeaderHome>
         <Routes>
           <Route path={'/'} element={<Home></Home>}></Route>
           <Route path='/busca/:code' element={<Busca></Busca>}></Route>
         </Routes>
+        <Footer></Footer>
       </Router>
     </>
   );
