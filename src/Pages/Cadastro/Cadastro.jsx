ae5081737d35b73a358d686c34259178ee0c2bfd
Anderson <ands1776@gmail.com>
2022-05-01 09:16:57 -0300

    Upando cadastro

---
+++ src/Pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Div, Form, Inputs, InputText, Texto, Botao } from './style';
+import { useNavigate } from "react-router-dom";
+import { cadastroForm } from '../../services/Api';
+
+const Cadastro = () => {
+  const [value, setValue] = useState({});
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  }
+  
+  console.log("value", value);
+
+  const Api = () => {
+    cadastroForm
+      .post("/cliente", value)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((erro) => console.log(erro));
+  };
+
+  function submit() {
+    Api();
+    navigate("/login");
+  }
+
+    return (
+      <Div> 
+            <Form onSubmit={submit}>
+              <Texto>Crie sua conta</Texto>
+              <Inputs>
+                <InputText type='text' placeholder='Nome Completo' onChange={handleChange} name='NOME_COMPLETO'/>
+                <InputText type='email' placeholder='E-mail' onChange={handleChange} name='EMAIL'/>
+                <InputText type='text' placeholder='CPF' onChange={handleChange} name='CPF'/>
+                <InputText type='tel' placeholder='Telefone' onChange={handleChange} name='TELEFONE'/>
+                <InputText type='password' placeholder='Senha' onChange={handleChange} name='SENHA'/>
+                <Botao type='submit'>Criar minha conta</Botao>
+              </Inputs>
+            </Form>
+        </Div>
+    );
+  };
+  
+  export default Cadastro;
