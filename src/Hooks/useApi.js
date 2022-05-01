e3f856e27eaf5e89b5f017c441f812c3578dd905
anaabalbi <anaabalbi@gmail.com>
2022-05-01 01:12:24 -0300

    limpando a useApi

--- src/Hooks/useApi.js
+++ src/Hooks/useApi.js
@@ -12,7 +12,6 @@ export function useApi(url) {
       .get(url)
       .then((response) => {
         setData(response.data.address.address[0]);
-        console.log(data);
         console.log(response.data);
       })
       .catch((err) => {
@@ -23,7 +22,7 @@ export function useApi(url) {
           setIsFetching(false);
         }, 3000);
       });
-  }, []);
+  }, [data]);
 
   return { data, error, isFetching };
 }
