c97c5e78bbad1d1693aafd026c9e97b32918feb0
anaabalbi <anaabalbi@gmail.com>
2022-05-01 22:13:26 -0300

    mudando a rota de importação do ContextUser

--- src/Pages/DashboardPerfil/Dashboard.jsx
+++ src/Pages/DashboardPerfil/Dashboard.jsx
@@ -1,14 +1,14 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { cadastroForm } from "../../services/Api.js";
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { cadastroForm } from '../../service/Api';
 import {
   Container,
   Inputs,
   Input,
   Buttons,
   Button,
-} from "./Dashboard-styled.jsx";
-import { ContextUser } from "../../providers/ContextUser.js";
+} from './Dashboard-styled.jsx';
+import { ContextUser } from '../../Provider/ContextUser';
 
 function Dashboard() {
   const { dataUser, setDataUser } = useContext(ContextUser);
@@ -17,16 +17,16 @@ function Dashboard() {
 
   function handleUpdate() {
     cadastroForm
-      .put("/cliente/id/" + dataUser.usuario.ID, dataUser.usuario)
+      .put('/cliente/id/' + dataUser.usuario.ID, dataUser.usuario)
       .then((response) => {
         console.log(response.data);
-        navigate("/login");
+        navigate('/login');
       });
   }
 
   function handleDelete() {
-    cadastroForm.delete("/cliente/id/" + dataUser.usuario.ID);
-    navigate("/login");
+    cadastroForm.delete('/cliente/id/' + dataUser.usuario.ID);
+    navigate('/login');
   }
 
   function handleOnChange(e) {
@@ -34,47 +34,49 @@ function Dashboard() {
   }
 
   return (
-    <Container>
-      <h1>Olá, {dataUser.usuario.NOME_COMPLETO}</h1>
-      <Inputs>
-        <h3>Informações Pessoais</h3>
-        <Input
-          type="text"
-          name="nome_completo"
-          onChange={handleOnChange}
-          placeholder={dataUser.usuario.NOME_COMPLETO}
-        />
-        <Input
-          type="email"
-          name="EMAIL"
-          placeholder={dataUser.usuario.EMAIL}
-          onChange={handleOnChange}
-        />
-        <Input
-          type="text"
-          name="CPF"
-          placeholder={dataUser.usuario.CPF}
-          onChange={handleOnChange}
-        />
-        <Input
-          type="tel"
-          name="TELEFONE"
-          placeholder={dataUser.usuario.TELEFONE}
-          onChange={handleOnChange}
-        />
-        <Input
-          type="password"
-          name="SENHA"
-          placeholder={dataUser.usuario.SENHA}
-          onChange={handleOnChange}
-        />
-      </Inputs>
+    <>
+      <Container>
+        <h1>Olá, {dataUser.usuario.NOME_COMPLETO}</h1>
+        <Inputs>
+          <h3>Informações Pessoais</h3>
+          <Input
+            type='text'
+            name='nome_completo'
+            onChange={handleOnChange}
+            placeholder={dataUser.usuario.NOME_COMPLETO}
+          />
+          <Input
+            type='email'
+            name='EMAIL'
+            placeholder={dataUser.usuario.EMAIL}
+            onChange={handleOnChange}
+          />
+          <Input
+            type='text'
+            name='CPF'
+            placeholder={dataUser.usuario.CPF}
+            onChange={handleOnChange}
+          />
+          <Input
+            type='tel'
+            name='TELEFONE'
+            placeholder={dataUser.usuario.TELEFONE}
+            onChange={handleOnChange}
+          />
+          <Input
+            type='password'
+            name='SENHA'
+            placeholder={dataUser.usuario.SENHA}
+            onChange={handleOnChange}
+          />
+        </Inputs>
 
-      <Buttons>
-        <Button onClick={handleUpdate}>Atualizar dados</Button>
-        <Button onClick={handleDelete}>Deletar conta</Button>
-      </Buttons>
-    </Container>
+        <Buttons>
+          <Button onClick={handleUpdate}>Atualizar dados</Button>
+          <Button onClick={handleDelete}>Deletar conta</Button>
+        </Buttons>
+      </Container>
+    </>
   );
 }
 
