4b35c58715ab830444f025498066933c58aeef89
anaabalbi <anaabalbi@gmail.com>
2022-05-01 22:13:58 -0300

    import header e footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import '../../App.css';
 import Cards from '../../components/CardsHome/Cards';
+import Footer from '../../components/Footer/Footer';
+import HeaderHome from '../../components/HeaderHome/HeaderHome';
 import HeroSection from '../../components/HeroSection/HeroSection';
 import SearchModal from '../../components/SearchModal/SearchModal';
 import { SearchModalContext } from '../../Provider/context-modal';
@@ -9,9 +11,11 @@ function Home() {
   const { openSearchModal } = useContext(SearchModalContext);
   return (
     <>
+      <HeaderHome></HeaderHome>
       <HeroSection />
       <Cards />
       {openSearchModal && <SearchModal></SearchModal>}
+      <Footer></Footer>
     </>
   );
 }
