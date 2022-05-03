5668657cef7bb1122e5dcfbd0215abb6e72aeb0d
anaabalbi <anaabalbi@gmail.com>
2022-05-02 21:56:25 -0300

    tirando o console.log do contextuser

--- src/Provider/ContextUser.js
+++ src/Provider/ContextUser.js
@@ -3,7 +3,6 @@ import { createContext, useState } from 'react';
 export const ContextUser = createContext();
 export const InfoUserProvider = (props) => {
   const [dataUser, setDataUser] = useState({});
-  console.log(dataUser);
   return (
     <ContextUser.Provider value={{ dataUser, setDataUser }}>
       {props.children}
